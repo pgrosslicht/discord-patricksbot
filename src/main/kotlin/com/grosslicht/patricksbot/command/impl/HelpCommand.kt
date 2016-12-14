41a7eaf3f3e6817cb263be776ae7666e89eb93c6
Patrick Grosslicht <patrick@grosslicht.com>
2016-12-14 14:22:19 +0100

    useless stuff

--- src/main/kotlin/com/grosslicht/patricksbot/command/impl/HelpCommand.kt
+++ src/main/kotlin/com/grosslicht/patricksbot/command/impl/HelpCommand.kt
@@ -7,15 +7,18 @@ import de.vandermeer.asciitable.v2.V2_AsciiTable
 import de.vandermeer.asciitable.v2.render.V2_AsciiTableRenderer
 import de.vandermeer.asciitable.v2.render.WidthAbsoluteEven
 import de.vandermeer.asciitable.v2.themes.V2_E_TableThemes
+import mu.KLogging
 import net.dv8tion.jda.core.MessageBuilder
+import net.dv8tion.jda.core.entities.Message
 
 /**
  * Created by patrickgrosslicht on 12/11/16.
  */
 class HelpCommand(private val commandHandler: CommandHandler) : CommandExecutor {
+    companion object: KLogging()
 
-    @Command(aliases = arrayOf(".help"), description = "Shows this page.")
-    fun onHelpCommand(style: String = "light"): String {
+    @Command(aliases = arrayOf(".help"), usage = ".help <theme>", description = "Shows this page.")
+    fun onHelpCommand(message: Message): String {
         val builder = MessageBuilder()
         val table = V2_AsciiTable()
         table.addRule()
@@ -28,12 +31,12 @@ class HelpCommand(private val commandHandler: CommandHandler) : CommandExecutor 
                     table.addRule()
                 }
         val renderer = V2_AsciiTableRenderer()
-        when (style) {
-            "double" -> renderer.setTheme(V2_E_TableThemes.UTF_DOUBLE.get())
-            "double-light" -> renderer.setTheme(V2_E_TableThemes.UTF_DOUBLE_LIGHT.get())
-            "strong-double" -> renderer.setTheme(V2_E_TableThemes.UTF_STRONG_DOUBLE.get())
-            "light-double" -> renderer.setTheme(V2_E_TableThemes.UTF_LIGHT_DOUBLE.get())
-            "heavy" -> renderer.setTheme(V2_E_TableThemes.UTF_HEAVY.get())
+
+        when {
+            message.content.contains("double-light") -> renderer.setTheme(V2_E_TableThemes.UTF_DOUBLE_LIGHT.get())
+            message.content.contains("light-double") -> renderer.setTheme(V2_E_TableThemes.UTF_LIGHT_DOUBLE.get())
+            message.content.contains("double") -> renderer.setTheme(V2_E_TableThemes.UTF_DOUBLE.get())
+            message.content.contains("heavy") -> renderer.setTheme(V2_E_TableThemes.UTF_HEAVY.get())
             else -> renderer.setTheme(V2_E_TableThemes.UTF_LIGHT.get())
         }
         renderer.setWidth(WidthAbsoluteEven(85))

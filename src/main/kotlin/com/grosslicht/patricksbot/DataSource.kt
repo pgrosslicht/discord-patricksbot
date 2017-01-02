0e365df569e171431213b22c2521550b03194af3
Patrick Grosslicht <patrick@grosslicht.com>
2017-01-03 00:36:52 +0100

    switched to mariadb jdbc

--- src/main/kotlin/com/grosslicht/patricksbot/DataSource.kt
+++ src/main/kotlin/com/grosslicht/patricksbot/DataSource.kt
@@ -1,20 +1,20 @@
 package com.grosslicht.patricksbot
 
 import com.grosslicht.patricksbot.models.Models
-import com.mysql.cj.jdbc.MysqlDataSource
 import io.requery.Persistable
 import io.requery.sql.KotlinConfiguration
 import io.requery.sql.KotlinEntityDataStore
+import org.mariadb.jdbc.MariaDbDataSource
 
 /**
  * Created by patrickgrosslicht on 02/01/17.
  */
 object DataSource {
-    val dataSource: MysqlDataSource = MysqlDataSource()
+    val dataSource: MariaDbDataSource = MariaDbDataSource()
     val data: KotlinEntityDataStore<Persistable>
         get() = KotlinEntityDataStore(KotlinConfiguration(dataSource = dataSource, model = Models.DEFAULT))
     init {
         val jdbcUrl = System.getenv("JDBC_URL")
-        dataSource.setURL(jdbcUrl)
+        dataSource.setUrl(jdbcUrl)
     }
 }

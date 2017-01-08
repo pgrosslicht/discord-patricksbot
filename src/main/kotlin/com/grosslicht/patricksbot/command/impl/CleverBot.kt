d34024eb8662dbde94c8b5439e4154459c43c61f
Patrick Grosslicht <patrick@grosslicht.com>
2017-01-08 15:52:25 +0100

    switched cleverbot api

--- src/main/kotlin/com/grosslicht/patricksbot/command/impl/CleverBot.kt
+++ src/main/kotlin/com/grosslicht/patricksbot/command/impl/CleverBot.kt
@@ -1,8 +1,8 @@
 package com.grosslicht.patricksbot.command.impl
 
-import com.github.kittinunf.fuel.httpPost
-import com.github.salomonbrys.kotson.fromJson
-import com.google.gson.Gson
+import com.google.code.chatterbotapi.ChatterBotFactory
+import com.google.code.chatterbotapi.ChatterBotSession
+import com.google.code.chatterbotapi.ChatterBotType
 import mu.KLogging
 import net.dv8tion.jda.core.entities.MessageChannel
 import java.util.concurrent.ThreadLocalRandom
@@ -12,49 +12,13 @@ import kotlin.concurrent.thread
  * Created by patrickgrosslicht on 08/01/17.
  */
 class CleverBot {
-    companion object : KLogging()
-    data class CreateRequest(val user: String, val key: String)
-    data class CreateResponse(val status: String, val nick: String)
-    data class AskRequest(val user: String, val key: String, val nick: String, val text: String)
-    data class AskResponse(val status: String, val response: String)
-
-    var nick = ""
-
-    init {
-        "https://cleverbot.io/1.0/create".httpPost().body(Gson().toJson(CreateRequest(System.getenv("CLEVERBOT_API_USER"), System.getenv("CLEVERBOT_API_KEY"))))
-                .header(mapOf("Content-Type" to "application/json", "Accept" to "application/json", "User-Agent" to "PatricksBot/CleverBot"))
-                .responseString { request, response, result ->
-                    result.fold({ d ->
-                        val createResponse = Gson().fromJson<CreateResponse>(d)
-                        if (createResponse.status == "success") {
-                            nick = createResponse.nick
-                        } else {
-                            logger.error { createResponse.status }
-                        }
-                    }, { err ->
-                        logger.error { err }
-                    })
-                }
-    }
+    val chatBotSession: ChatterBotSession = ChatterBotFactory().create(ChatterBotType.CLEVERBOT).createSession()
 
     fun ask(question: String, channel: MessageChannel) {
         thread {
             channel.sendTyping().queue()
-            Thread.sleep(ThreadLocalRandom.current().nextLong(1000, 5000))
-            "https://cleverbot.io/1.0/ask".httpPost().body(Gson().toJson(AskRequest(System.getenv("CLEVERBOT_API_USER"), System.getenv("CLEVERBOT_API_KEY"), nick, question)))
-                    .header(mapOf("Content-Type" to "application/json", "Accept" to "application/json", "User-Agent" to "PatricksBot/CleverBot"))
-                    .responseString { request, response, result ->
-                        result.fold({ d ->
-                            val createResponse = Gson().fromJson<AskResponse>(d)
-                            if (createResponse.status == "success") {
-                                channel.sendMessage(createResponse.response).queue { }
-                            } else {
-                                logger.error { createResponse.status }
-                            }
-                        }, { err ->
-                            logger.error { err }
-                        })
-                    }
+            Thread.sleep(ThreadLocalRandom.current().nextLong(1000, 4000))
+            channel.sendMessage(chatBotSession.think(question)).queue()
         }
     }
 }
